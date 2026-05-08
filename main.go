680708e18f067f41a3a8ff39731e872b8a1e2f4d
tonah <onahpatience1610@gmail.com>
2026-05-08 11:51:03 +0100

    modified the main function and the parsearg function

--- main.go
+++ main.go
@@ -3,23 +3,28 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
 	"os"
 )
-func ParseArgs(args []string) (string, string, error){
+var errorMessage error = errors.New("Error: Please provide exactly one input file and one output file")
+func ParseArgs(args []string) ( inputFile string, outputFile string, err error){
 	/*parse args receives a slice of strings and 
 	 return 2 strings and an error*/
 	if len(args) != 2 {
-		return "", "", errors.New("Error Message.")
+		return "", "", errorMessage
 	}
-	return "","", nil
+	return args[0],args[1], nil
 }
 
 func main() {
 	//the main function needs to retrieve string from stdin uisng os pkg
 	inputArgs := os.Args[1:]
-	a, b, err := ParseArgs(inputArgs)
-	fmt.Println(a)
-	fmt.Println(b)
+	inputFile, outputFile, err := ParseArgs(inputArgs)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	fmt.Println(inputFile)
+	fmt.Println(outputFile)
 	fmt.Println(err)
 	
 }
